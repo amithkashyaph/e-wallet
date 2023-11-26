a61e3292ae5c37252f8abf6ccde8e6f49f550e4a
amithkashyap <amithkashyaprv@gmail.com>
2023-11-26 14:11:56 +0530

    Added Commons module to store commonly used data

---
+++ Commons/src/main/java/com/project/Commons/Constants.java
@@ -0,0 +1,23 @@
+package com.project.Commons;
+
+public class Constants {
+
+    public static final String USER_CREATION_TOPIC = "user_created";
+
+    public static final String TRANSACTION_INITIATION_TOPIC = "transaction_initiated";
+
+    public static final String TRANSACTION_UPDATION_TOPIC = "transaction_update";
+
+    public static final String USER_CREATION_TOPIC_USER_ID = "user_id";
+
+    public static final String USER_CREATION_TOPIC_CONTACT = "contact";
+
+    public static final String USER_CREATION_TOPIC_EMAIL = "email";
+
+    public static final String USER_CREATION_TOPIC_USER_IDENTIFIER = "userIdentifier";
+
+    public static final String USER_CREATION_TOPIC_USER_IDENTIFIER_VALUE = "userIdentifierValue";
+
+    public static final String USER_CREATION_TOPIC_USER_NAME = "name";
+
+}
---
+++ Commons/src/main/java/com/project/Commons/UserIdentifier.java
@@ -0,0 +1,7 @@
+package com.project.Commons;
+
+public enum UserIdentifier {
+    PAN,
+    AADHAR_CARD,
+    VOTER_ID;
+}
